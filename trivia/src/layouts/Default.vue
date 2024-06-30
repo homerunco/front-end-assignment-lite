b5fb4b1af5160a81882695654970b84439f330bb
Linh Nguyen <mlinhng@gmail.com>
2024-06-30 07:19:39 +0200

    Create a default layout

---
+++ trivia/src/layouts/Default.vue
@@ -0,0 +1,15 @@
+<template>
+  <main :class="$style.layout">
+    <slot />
+  </main>
+</template>
+
+<style module>
+.layout {
+  max-width: 500px;
+  width: 100%;
+  height: 90vh;
+  background: white;
+  border-radius: var(--border-radius);
+}
+</style>
